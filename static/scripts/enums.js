b1686dcdd7ce2687ffbaebeadfb1a5ded8a7eea1
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-02 00:22:00 -0600

    update

---
+++ static/scripts/enums.js
@@ -0,0 +1,14 @@
+const Status = Object.freeze({
+    "Inactive": "-",
+    "AwaitingOrder": "Awaiting order",
+    "OrderPending": "Order received",
+    "OrderCooking": "Order cooking",
+    "AwaitingPayment": "Awaiting payment",
+    "PaymentReceived": "Payment received"
+});
+
+const Request = Object.freeze({
+    "None": "-",
+    "Refill": "Needs refill",
+    "Help": "Needs assistance"
+});
